cede4ebb9da277a74566eaf4f74d3f372f9f2462
eeubanks <shrimptankzilla@gmail.com>
2020-12-27 18:00:06 -0500

    intial commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  provisioner "local-exec" {
+      // Grab ip address of new instance
+      command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project" { 
+    type = string
+    default = "goodcopy"
+}
+
+variable "credentials_file" { 
+    default = "goodcopy-8ff1b72a9b34.json"
+}
+
+variable "region" {
+    default = "us-central1"
+}
+
+variable "zone" {
+    default = "us-central1-c"
+}
